f16592dc7faccc74c55741361a400550b36d9377
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2025-11-09 11:17:36 +0200

    Add project files.

---
+++ Couriers.Common/Extensions/HttpRequestResultExtensions.cs
@@ -0,0 +1,56 @@
+﻿using Couriers.Common.ResultTypes;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Couriers.Common.Extensions
+{
+    /// <summary>
+    /// Contains the extensions methods related to the <see cref="HttpRequestResult"/>
+    /// </summary>
+    public static class HttpRequestResultExtensions
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Creates an returns a new instance of <see cref="HttpRequestResult{T}"/> from the specified <paramref name="result"/>
+        /// </summary>
+        /// <typeparam name="TResult">The type of the new result</typeparam>
+        /// <param name="result">The result</param>
+        /// <param name="errorMessage">The error message</param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException">An exception is thrown if the <paramref name="result"/> is successful</exception>
+        public static HttpRequestResult<TResult> ToUnsuccessfulHttpRequestResult<TResult>([NotNull] this IHttpRequestResult result, string? errorMessage = null)
+        {
+#if NET6_0_OR_GREATER
+            ArgumentNullException.ThrowIfNull(result);
+#else
+            if (result is null)
+                throw new ArgumentNullException(nameof(result));
+#endif
+
+            if (result.IsSuccessful)
+                throw new InvalidOperationException($"The specified '{nameof(result)}' is successful.");
+
+            var resultErrorMessage = string.IsNullOrWhiteSpace(errorMessage) ? result.ErrorMessage : errorMessage;
+
+            return new HttpRequestResult<TResult>(resultErrorMessage, result.RequestPayload, result.ResponsePayload);
+        }
+
+        /// <summary>
+        /// Creates an returns a new instance of <see cref="HttpRequestResult{T}"/> from the specified <paramref name="result"/>
+        /// </summary>
+        /// <typeparam name="T">The type of the result</typeparam>
+        /// <typeparam name="TResult">The type of the new result</typeparam>
+        /// <param name="result">The result</param>
+        /// <param name="errorMessage">The error message</param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException">An exception is thrown if the <paramref name="result"/> is successful</exception>
+        public static HttpRequestResult<TResult> ToUnsuccessfulHttpRequestResult<T, TResult>([NotNull] this IHttpRequestResult<T> result, string? errorMessage = null)
+        {
+            return result.ToUnsuccessfulHttpRequestResult<TResult>(errorMessage);
+        }
+
+        #endregion
+    }
+}
---
+++ Couriers.Common/ResultTypes/IHttpRequestResult.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Couriers.Common.ResultTypes
+{
+    /// <summary>
+    /// Provides abstraction for the result of a HTTP request
+    /// </summary>
+    public interface IHttpRequestResult
+    {
+        #region Properties
+
+        /// <summary>
+        /// The error message
+        /// </summary>
+        string? ErrorMessage { get; }
+
+        /// <summary>
+        /// A flag indicating whether the operation was successful
+        /// </summary>
+        [MemberNotNullWhen(false, nameof(ErrorMessage))]
+        bool IsSuccessful { get; }
+
+        /// <summary>
+        /// The request payload
+        /// </summary>
+        string? RequestPayload { get; }
+
+        /// <summary>
+        /// The response payload
+        /// </summary>
+        string? ResponsePayload { get; }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Provides abstraction for the result of a HTTP request that returns a <typeparamref name="T"/>
+    /// </summary>
+    /// <typeparam name="T">The result of the operation</typeparam>
+    public interface IHttpRequestResult<out T> : IHttpRequestResult
+    {
+        #region Properties
+
+        /// <summary>
+        /// The result of the operation
+        /// </summary>
+        public T Result { get; }
+
+        #endregion
+    }
+}
